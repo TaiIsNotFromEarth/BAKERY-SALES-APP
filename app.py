6bbce68c282b624e536928d2804181633dbac521
TaiIsNotFromEarth <atolanishennuwa9@gmail.com>
2024-08-09 11:28:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 
 def load_data():
-    file = 'Bakeryapp/Bakery-sales.csv'
+    file = 'Bakery sales.csv'
     df = pd.read_csv(file)
     df.rename(columns={'Unnamed: 0': 'id',
                 'article':'product',
